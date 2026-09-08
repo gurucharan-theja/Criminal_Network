c2f8b15b6de13f9445e775158435e1dd2faca32a
gurucharan-theja <gurucharan9405@gmail.com>
2026-09-08 13:42:31 +0530

    fix: remove UTF-8 BOM from WebConfig.java

--- backend/src/main/java/com/sih/criminalnetwork/config/WebConfig.java
+++ backend/src/main/java/com/sih/criminalnetwork/config/WebConfig.java
@@ -1,4 +1,4 @@
-﻿package com.sih.criminalnetwork.config;
+package com.sih.criminalnetwork.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
